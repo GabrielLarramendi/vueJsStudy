008b6f4c1c7a8d934ce117e80b4745c00093ed5e
Gabriel Larramendi <larramendiga@gmail.com>
2023-06-03 02:07:50 -0300

    Creating the Vue App

---
+++ main.js
@@ -0,0 +1,9 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            userName: 'Gabriel',
+            email: 'gabriel@gmail.com',
+            birth: '13/03/1998'
+        }
+    }
+})
