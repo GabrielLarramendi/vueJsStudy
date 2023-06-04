d7b8fe8616833008fe6956e7bd618d2c4f994d79
Gabriel Larramendi <larramendiga@gmail.com>
2023-06-04 03:03:18 -0300

    Event Handling

--- main.js
+++ main.js
@@ -1,11 +1,32 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart: 0,
             product: 'Socks',
             image: "./assets/images/socks_green.jpg",
-            url: "https://www.globo.com/",
-            inventory: 11,
-            onSale: true
+            inStock: false,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {id: 2234, color: 'green', image:'./assets/images/socks_green.jpg'},
+                {id: 2235, color: 'blue', image:'./assets/images/socks_blue.jpg'}
+            ]
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        removeFromCart() {
+            if (this.cart > 0) {
+                this.cart -= 1
+            } 
+            else {
+                this.cart = 0
+            }
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
         }
     }
+
 })
