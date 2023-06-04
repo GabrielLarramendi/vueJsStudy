655fb2397fcd6bab33745638644b7c0a06e6696a
Gabriel Larramendi <larramendiga@gmail.com>
2023-06-04 18:52:18 -0300

    Computed Properties

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 const app = Vue.createApp({
     data() {
         return {
+            onSale: true-,
             cart: 0,
             product: 'Socks',
-            image: "./assets/images/socks_green.jpg",
-            inStock: false,
+            brand: 'Vue Mastery',
+            selectedVariant: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                {id: 2234, color: 'green', image:'./assets/images/socks_green.jpg'},
-                {id: 2235, color: 'blue', image:'./assets/images/socks_blue.jpg'}
+                {id: 2234, color: 'green', image:'./assets/images/socks_green.jpg', quantity: 50},
+                {id: 2235, color: 'blue', image:'./assets/images/socks_blue.jpg', quantity: 0}
             ]
         }
     },
@@ -24,8 +25,26 @@ const app = Vue.createApp({
                 this.cart = 0
             }
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.selectedVariant = index
+        }
+    },
+    computed: {
+        title() {
+            title = this.brand + ' ' + this.product
+            if (this.onSale) {
+                return  title + ' is on sale!'
+            }
+            else {
+                return title + ' is NOT on sale!'
+            }
+            
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
         }
     }
 
